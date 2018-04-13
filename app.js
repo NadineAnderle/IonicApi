d44b837cfa77a13ffb5c4e8670e2c6f279c51045
Nadine <nadianderle@gmail.com>
2018-04-12 21:13:06 -0300

    produtos

--- app.js
+++ app.js
@@ -40,5 +40,72 @@ app.post('/api/login', function(req,res){
     }
 });
 
+app.get('/api/produtos',function(req,res){
+    if(req.header('token')!='este_e_o_token'){
+        
+        setTimeout(function(){
+            res.send(401,{
+                'erro':{
+                    'http_code':401,
+                    'code':'unauthorized',
+                    'menssagem':'Login não autorizado'
+                }
+            })
+        },
+         4000);
+    }else{
+        setTimeout(() => {
+            res.header('Access-Control-Allow-Origin','*')
+            .send(200,{
+                data:{
+                    produtos:[
+                        {
+                            id:1,
+                            nome:'Chocolate',
+                            categoria:'doces',
+                            descricao:'meio amorgo'
+                        },
+                        {
+                            id:2,
+                            nome:'Coxinha',
+                            categoria:'salgados',
+                            descricao:'é bom'
+                        },
+                        {
+                            id:3,
+                            nome:'açai',
+                            categoria:'doces',
+                            descricao:'tem quem gosta tem quem não'
+                        },
+                        {
+                            id:4,
+                            nome:'BALA',
+                            categoria:'doces',
+                            descricao:'GOMA'
+                        },
+                        {
+                            id:5,
+                            nome:'BANANA',
+                            categoria:'FRUTA',
+                            descricao:'É SAUDAVEL'
+                        },
+                        {
+                            id:6,
+                            nome:'ARROZ',
+                            categoria:'INTEGRAIS',
+                            descricao:'INTEGRAIS'
+                        },
+                        {
+                            id:1,
+                            nome:'SUCO',
+                            categoria:'BEBIDAS',
+                            descricao:'SUCO NATURAL'
+                        }
+                    ]
+                }
+            })
+        }, 4000);
+    }
+});
 app.listen(3000);
 console.log('A API esta online')
